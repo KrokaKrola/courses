cf32d7ef59848dbb846f2f5935471f1a76e9a9cb
Oleg <krokakrola@gmail.com>
2019-11-12 19:19:51 +0300

    merge

---
+++ Js intense/lesson_5/es6/lesson8/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Articles from './articles';
+// import './test-decorators';
+
+import Site from './components/site';
+ReactDOM.render(<Site/>, document.querySelector('.app'));
