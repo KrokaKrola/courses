69c69726f2ded4411294e5eb3c2c2c78368fb8b3
KrokaKrola <harlamsan@gmail.com>
2019-11-12 10:36:44 +0300

    work commit

--- Js intense/lesson_5/es6/lesson7_3/articles.js
+++ Js intense/lesson_5/es6/lesson7_3/articles.js
@@ -13,6 +13,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 
+
 export default class extends React.Component {
   constructor(props) {
     super(props);
