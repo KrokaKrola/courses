0d0b2d23195f986fc8264748c5db636a163b930c
KrokaKrola <harlamsan@gmail.com>
2019-11-22 00:53:19 +0300

    merging

--- pure-react/src/counter/index.js
+++ pure-react/src/counter/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+
+//1. Mount
+//2. Render
+//3. Commit
+//4. Unmount
+
+class OldValue extends React.PureComponent {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if(nextProps.value === this.props.value) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  render() {
+    console.log('[OldValue] rendered');
+    return <div>{this.props.value}</div>;
+  }
+}
 
 class Counter extends React.Component {
   state = {
     wrap: {
       counter: 0,
-      lit: false
+      lit: false,
+      oldValues: []
     }
   };
+
+  // componentDidMount() {
+  //   console.log('[componentDidMount]', 'Mounted.');
+  //   //fetch data, add event listeners, DOM nodes
+  // }
+
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   console.log(prevProps, prevState, snapshot);
+  //   //re-sync
+  // }
+
+  // // UNSAFE_componentWillMount() {
+  // //   console.log('[componentWillMount]', 'Goodbye.');
+  // //   //cleanup - timers, subsctiption
+  // // }
 
   increment() {
     this.setState(
@@ -17,12 +52,16 @@ class Counter extends React.Component {
         return {
           wrap: {
             ...previousState.wrap,
-            counter: previousState.wrap.counter + 1
+            counter: previousState.wrap.counter + 1,
+            oldValues: [
+              ...this.state.wrap.oldValues,
+              previousState.wrap.counter + 1
+            ]
           }
         };
       },
       () => {
-        console.log(this.state);
+        // console.log(this.state);
       }
     );
   }
@@ -33,7 +72,11 @@ class Counter extends React.Component {
         return {
           wrap: {
             ...previousState.wrap,
-            counter: previousState.wrap.counter - 1
+            counter: previousState.wrap.counter - 1,
+            oldValues: [
+              ...this.state.wrap.oldValues,
+              previousState.wrap.counter - 1
+            ]
           }
         };
       },
@@ -44,26 +87,35 @@ class Counter extends React.Component {
   }
 
   toggle = () => {
-    this.setState(previousState => ({ 
+    this.setState(previousState => ({
       wrap: {
-      ...previousState.wrap,
-      lit: !previousState.wrap.lit
-    } 
-  }));
+        ...previousState.wrap,
+        lit: !previousState.wrap.lit
+      }
+    }));
   };
 
   render() {
     const { lit, counter } = this.state.wrap;
     return (
-      <div className={`counter ${lit ? '' : 'dark'}`}>
-        <h2>Counter</h2>
-        <div>
-          <button onClick={this.decrement.bind(this)}>-</button>
-          <span className='count'>{counter}</span>
-          <button onClick={this.increment.bind(this)}>+</button>
+      <>
+        <div className={`counter ${lit ? '' : 'dark'}`}>
+          <h2>Counter</h2>
+          <div>
+            <button onClick={this.decrement.bind(this)}>-</button>
+            <span className='count'>{counter}</span>
+            <button onClick={this.increment.bind(this)}>+</button>
+          </div>
+          <button onClick={this.toggle}>Light/Dark</button>
         </div>
-        <button onClick={this.toggle}>Light/Dark</button>
-      </div>
+        <ul>
+          {this.state.wrap.oldValues.map((value, index) => (
+            <li key={index}>
+              <OldValue value={value} />
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
