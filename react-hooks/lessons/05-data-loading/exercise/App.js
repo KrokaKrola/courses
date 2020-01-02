d682e36a1744268f04e581e7c38e8a8e24890abf
KrokaKrola <harlamsan@gmail.com>
2020-01-02 12:21:11 +0300

    useEffect hook

--- react-hooks/lessons/05-data-loading/exercise/App.js
+++ react-hooks/lessons/05-data-loading/exercise/App.js
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 
-import { onAuthStateChanged } from 'app/utils'
-import LoggedIn from 'app/LoggedIn'
-import LoggedOut from 'app/LoggedOut'
+import { onAuthStateChanged } from "app/utils"
+import LoggedIn from "app/LoggedIn"
+import LoggedOut from "app/LoggedOut"
+
+function useAuth() {
+  const [auth, setAuth] = useState(null)
+  const [authAttempted, setAuthAttempted] = useState(false)
+  useEffect(() => {
+     return onAuthStateChanged(auth => {
+      setAuth(auth)
+      setAuthAttempted(true)
+    })
+  }, [])
+  return { auth, authAttempted }
+}
 
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const { auth, authAttempted } = useAuth()
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
@@ -14,7 +25,7 @@ export default function App() {
 
   return (
     <div className="Layout">
-      {auth ? <LoggedIn auth={auth} /> : <LoggedOut />}
+      {auth ? <LoggedIn auth={auth}/> : <LoggedOut/>}
     </div>
   )
 }
